e9b2f3d8e6782c02b64657eedb056c4641ca8f53
Alan Robertson <alan@qrop.xyz>
2025-06-18 14:20:48 +1000

    Linting cache tests

--- src/pyLIQTR/utils/tests/test_cached.py
+++ src/pyLIQTR/utils/tests/test_cached.py
@@ -1,23 +1,11 @@
 '''
     Tests for the Deferred Bloq
 '''
-import types
 import unittest
 
-from functools import partial
-
 import cirq
-from qualtran import CompositeBloq
-from pyLIQTR.utils.deferred import Cached 
-from pyLIQTR.utils.repeat import circuit_to_quregs
-
-from pyLIQTR.utils.circuit_decomposition import circuit_decompose_multi
-from pyLIQTR.utils.circuit_decomposition import generator_decompose
-
+from pyLIQTR.utils.deferred import Cached
 from pyLIQTR.utils.tests.test_helpers import TestHelpers, extract_and_run_tests
-
-from qualtran.bloqs.basic_gates import CNOT, Hadamard
-from qualtran import BloqBuilder
 
 
 class TestDeferredBloq(unittest.TestCase, TestHelpers):
@@ -25,7 +13,9 @@ class TestDeferredBloq(unittest.TestCase, TestHelpers):
         Tests for the Deferred Bloq
     '''
     def test_cirq_unary_gate(self, n_qubits=10):
-
+        '''
+            Testing a cirq gate
+        '''
         q = [cirq.LineQubit(i) for i in range(n_qubits)]
         target_gate = cirq.H
 
@@ -33,14 +23,15 @@ class TestDeferredBloq(unittest.TestCase, TestHelpers):
         assert next(gate.compose()) == target_gate(q[0])
 
         # Tag is already set, the cache should return a non-0 qubit object here
-        for i in range(1, n_qubits): 
-            assert not (next(gate.compose()) == target_gate(q[i]))
+        for i in range(1, n_qubits):
+            assert not next(gate.compose()) == target_gate(q[i])
 
         # Tag is already set, the cache should return a non-0 qubit object here
-        for i in range(1, n_qubits): 
+        for i in range(1, n_qubits):
             gate = Cached(f'tag_{i}', target_gate, q[0])
 
-            assert not (next(gate.compose()) == target_gate(q[i]))
+            assert not next(gate.compose()) == target_gate(q[i])
+
 
 # Test runner without invoking subprocesses
 # Used for interactive and pdb hooks
